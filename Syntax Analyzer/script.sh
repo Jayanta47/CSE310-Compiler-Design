b7233814ba76e235727fe27e94dc60edfe5374a7
Jayanta47 <jayantasadhu4557@gmail.com>
2021-04-23 15:58:43 +0600

    gitignore modified for Syntax Analyzer

---
+++ Syntax Analyzer/script.sh
@@ -0,0 +1,9 @@
+bison -d -y -v 1705047.y
+echo "Bison File produced"
+g++ -w -c -o y.o y.tab.c
+flex -o ./lex.yy.c ./1705047.l
+g++ -w -c -o l.o lex.yy.c
+echo "lex file compiled and output created"
+echo "Creating linker"
+g++ ./y.o ./l.o -lfl 
+./a.out input1.txt log.txt error.txt
